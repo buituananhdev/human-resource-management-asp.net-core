2de17b272dcf87ac953c99d5b6be88899197264c
TuanAnhDepTrai <anhaanh2003@gmail.com>
2023-04-14 13:58:36 +0700

    feat CRUD Department

--- HRM/Services/DepartmentService/IDepartmentService.cs
+++ HRM/Services/DepartmentService/IDepartmentService.cs
@@ -3,6 +3,7 @@
     public interface IDepartmentService
     {
         Task<List<Department>> GetAllDepartments();
+        Task<Department?> GetSingleDepartment(string id);
         Task<List<Department>> AddDepartment(Department department);
         Task<List<Department>?> UpdateDepartment(string id, Department request);
         Task<List<Department>?> DeleteDepartment(string id);
